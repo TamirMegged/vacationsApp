a39202c0955ee771881d8f130dd3bf7c4402a5ba
TamirMegged <tamir.m.nx@gmail.com>
2020-10-16 20:35:23 +0300

    build in the server folder

---
+++ server/build/precache-manifest.c806df41745d3e7753bb9e3bcf1cc9c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe385d6e2acb1db03bd9cfd4d399eed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81942ebcea2edbd2be7c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "380549e83cbf1160abb1",
+    "url": "/static/js/2.7fec9376.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/static/js/2.7fec9376.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81942ebcea2edbd2be7c",
+    "url": "/static/js/main.6864fcf3.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "c2982aa31a75bb4c4f123672f0fa4847",
+    "url": "/static/media/logo.c2982aa3.png"
+  },
+  {
+    "revision": "4ce37a351178ec7e4e740a8e98f6241d",
+    "url": "/static/media/world.4ce37a35.png"
+  }
+]);
--- server/index.js
+++ server/index.js
@@ -11,10 +11,10 @@ app.use("/vacations", require('./routes/vacations'));
 app.use("/likes", require('./routes/likes'));
 
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port, () => console.log(`up and running on ${port}`));
