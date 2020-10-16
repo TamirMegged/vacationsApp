9909f2594401e1535c4a11cd7304aa58611aa977
TamirMegged <tamir.m.nx@gmail.com>
2020-10-16 20:13:22 +0300

    initial commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 1000;
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+app.use("/users", require('./routes/users'));
+app.use("/vacations", require('./routes/vacations'));
+app.use("/likes", require('./routes/likes'));
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, '..', 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
+
+app.listen(port, () => console.log(`up and running on ${port}`));
